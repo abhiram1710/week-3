32b4c3db2e57ecd5934f4abc60b460756358a571
vikramdev772 <vikramdev772@gmail.com>
2026-01-07 10:54:21 +0530

    signup

--- myapp/src/main/java/com/example/myapp/controller/AuthController.java
+++ myapp/src/main/java/com/example/myapp/controller/AuthController.java
@@ -2,13 +2,19 @@ package com.example.myapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myapp.dto.SignupRequest;
 
 @RestController
 public class AuthController {
     @PostMapping("/signup")
-    String signUp(){
-        return "signup sucess";
+    String signUp(@RequestBody SignupRequest sd){
+        System.out.println("\n\t signup DATA : "+sd.toString());
+        return "signup sucess -> : { \n\t name :"+sd.getName()+"\n\t email : "+sd.getEmail()+"\n}";
 
     }
+
+    
 }
